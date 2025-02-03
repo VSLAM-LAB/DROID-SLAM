1f51547991d21e647910060bf04e5188bd531823
alejandrofontan <fontanvillacampa@gmail.com>
2025-02-03 16:39:56 +1000

    script to download droid.pth from hugginface vslamlab repo

---
+++ vslamlab_droidslam_download_weights.py
@@ -0,0 +1,16 @@
+import os
+from zipfile import ZipFile
+from huggingface_hub import hf_hub_download
+
+
+def droidslam_download_weights():
+    # Download droid.pth
+    if not os.path.exists('droid.pth'):
+        file_path = hf_hub_download(repo_id='vslamlab/droidslam', filename='droid.pth', repo_type='dataset',
+                                    local_dir='.')
+        with ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall('.')
+
+
+if __name__ == "__main__":
+    droidslam_download_weights()
